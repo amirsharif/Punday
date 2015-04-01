1f09c3cf9a5b3e1669de62f0f1345651f688d43b
Amir Sharif <amirksharif@gmail.com>
2015-03-31 23:41:18 -0400

    Outlined rough pinger for checking guesses

---
+++ Rough answer pinger.py
@@ -0,0 +1,44 @@
+from mechanize import *
+
+url = 'http://www.mondaypunday.com'
+guess = 'keeping up with the jones'
+
+br = Browser()
+br.open(url)
+
+
+br.select_form(nr=0)
+br['answer'] = 'poop'
+resp = br.submit()
+
+import datetime
+
+print 'WAHOO' , datetime.datetime.now()
+
+html = resp.read()
+
+print 'YOOHOO' , datetime.datetime.now()
+
+try: 
+    html.index('Correct!')
+    print 'yay!!!!!!!!!!!!!!!'
+    print 'Zumba' , datetime.datetime.now()
+except:
+    print 'GROAHOO' , datetime.datetime.now()
+    print 'nice try'
+    br.select_form(nr=0)
+    print 'NAONO' , datetime.datetime.now()
+    br['answer'] = guess
+    print 'TOOTOOTOO' , datetime.datetime.now()
+    resp = br.submit()
+    print 'WAHOO' , datetime.datetime.now()
+
+    html = resp.read()
+
+    print 'YOOHOO' , datetime.datetime.now()
+    try: 
+        html.index('Correct!')
+        print 'yay!!!!!!!!!!!!!!!'
+        print 'Zumba' , datetime.datetime.now()
+    except:
+        print 'oops'
